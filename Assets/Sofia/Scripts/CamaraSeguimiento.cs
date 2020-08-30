96750c25b7489f18a2e1110d5d57cc86c9a8a3dd
SofieTorch <sofie.torch@outlook.com>
2020-08-30 13:30:11 -0400

    Camera follow

---
+++ Assets/Sofia/Scripts/CamaraSeguimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSeguimiento : MonoBehaviour
+{
+    public GameObject follow = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = follow.transform.position.x + 7f;
+        transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+    }
+}
