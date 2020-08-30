4362860bd32f2eddc61549df9fb60e3ef708ed98
Vale <delgadillo.escobar.valeria27@gmail.com>
2020-08-30 03:37:37 -0400

    Escenario Dos

---
+++ Assets/avanzandoSkeleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avanzandoSkeleton : MonoBehaviour
+{
+    public float vel = -1f;
+    Rigidbody2D rgb;
+
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v = new Vector2(vel, 0);
+        rgb.velocity = v;
+    }
+}
