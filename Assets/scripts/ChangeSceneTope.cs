a21f6dac91b3bec501026e5764c33cacbf75c57a
SofieTorch <sofie.torch@outlook.com>
2020-10-26 21:11:50 -0400

    Final version, we guess

---
+++ Assets/scripts/ChangeSceneTope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ChangeSceneTope : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject artemisa = default;
+    [SerializeField]
+    int numScene;
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject == artemisa){
+            SceneManager.LoadScene(numScene);
+        }
+    }
+}
